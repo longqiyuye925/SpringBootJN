e03de460ae2b746950ddb16107fff50e65ac1a0b
741722079@qq.com <741722079@qq.com>
2020-04-28 23:10:45 +0800

    更新

---
+++ springbootdemo/src/main/java/com/bjsxt/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.bjsxt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @author: zf
+ * @Param:
+ * @Return:
+ * @Date: 2020 04 2020/4/28
+ */
+@RestController
+@RequestMapping("/logback")
+public class HelloController {
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/showInfo")
+    public String showInfo() {
+        logger.debug("记录日志");
+        return "Hello Logback";
+    }
+}
