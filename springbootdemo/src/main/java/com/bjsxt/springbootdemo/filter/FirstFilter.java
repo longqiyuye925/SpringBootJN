cead7556bf05f06e8b95bbc2b1e179e4125177e8
741722079@qq.com <741722079@qq.com>
2020-05-07 21:49:06 +0800

    把listener集成到SpringBoot中

--- springbootdemo/src/main/java/com/bjsxt/springbootdemo/filter/FirstFilter.java
+++ springbootdemo/src/main/java/com/bjsxt/springbootdemo/filter/FirstFilter.java
@@ -1,11 +1,36 @@
 package com.bjsxt.springbootdemo.filter;
 
+import org.omg.CosNaming.NameComponent;
+
+import javax.naming.Name;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
 /**
- * @Description:
- * @author: zf
+ * @Description: 采用注解的方式注册filter
+ * @author: wsc
  * @Param:
  * @Return:
  * @Date: 2020 05 2020/5/7
  */
-public class FirstFilter {
+//@WebFilter(filterName = "FirstFilter",urlPatterns = {"*.do","*.jsp"})
+@WebFilter(filterName = "FirstFilter", urlPatterns = "/first")
+public class FirstFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("进入FirstFilter");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("离开FirstFilter");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
